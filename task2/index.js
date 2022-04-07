f8d496b13a76724268247fe214b50d49df8c29d9
Sveta Shish <sviatlanashysh@gmail.com>
2022-04-07 17:24:10 +0300

    review html and closing modal window

--- task2/index.js
+++ task2/index.js
@@ -1,21 +1,36 @@
 let buttonLogin = document.querySelector('.login');
 let buttonRegistration = document.querySelector('.registation');
 
-let wrapper =  document.querySelector('.wrapper');
-let container =document.querySelector('.container');
+let container = document.querySelector('.container');
+let modal =  document.querySelectorAll('.modal');
 let modalLogin = document.querySelector('.modal-window__login');
 let modalRegistration = document.querySelector('.modal-window__registration');
 
+let closeButton = document.querySelectorAll('.close');
 
+if (modal.length > 0) {
+  buttonLogin.addEventListener('click', () => {
+    modal[0].style.display = 'block';
+    container.style.display = 'none';
+  });
+  
+  buttonRegistration.addEventListener('click', () => {
+    modal[1].style.display = 'block';
+    container.style.display = 'none';
+  });
 
-buttonLogin.addEventListener('click', () => {
-  wrapper.style.backgroundColor = 'rgba(138, 133, 133, 0.849)';
-  modalLogin.style.display = 'flex';
-  container.style.display = 'none';
-})
-
-buttonRegistration.addEventListener('click', () => {
-  wrapper.style.backgroundColor = 'rgba(138, 133, 133, 0.849)';
-  modalRegistration.style.display = 'flex';
-  container.style.display = 'none';
-})
+  for (let i = 0; i < modal.length; i++){
+    closeButton[i].addEventListener('click', () => {
+      modal[i].style.display = 'none';
+      container.style.display = 'flex';
+      });
+  }
+  window.onclick = function(event) {
+    for (let i = 0; i < modal.length; i++){
+      if (event.target == modal[i]) {
+        modal[i].style.display = "none";
+        container.style.display = 'flex';
+      }
+    }
+  }
+}
