669a88719bb6f080ef7fa36c2129d92cfa20c0dc
Sveta Shish <sviatlanashysh@gmail.com>
2022-04-07 18:16:17 +0300

    add function for modal

--- task2/index.js
+++ task2/index.js
@@ -8,29 +8,28 @@ let modalRegistration = document.querySelector('.modal-window__registration');
 
 let closeButton = document.querySelectorAll('.close');
 
+const openModal = (element) => {
+  element.style.display = 'block';
+  container.style.display = 'none';
+};
+
+const closeModal = (element) => {
+  element.style.display = 'none';
+  container.style.display = 'flex';
+};
+
 if (modal.length > 0) {
-  buttonLogin.addEventListener('click', () => {
-    modal[0].style.display = 'block';
-    container.style.display = 'none';
-  });
+  buttonLogin.addEventListener('click', () => openModal(modal[0]))
   
-  buttonRegistration.addEventListener('click', () => {
-    modal[1].style.display = 'block';
-    container.style.display = 'none';
-  });
+  buttonRegistration.addEventListener('click', () => openModal(modal[1]))
 
   for (let i = 0; i < modal.length; i++){
-    closeButton[i].addEventListener('click', () => {
-      modal[i].style.display = 'none';
-      container.style.display = 'flex';
-      });
-  }
-  window.onclick = function(event) {
-    for (let i = 0; i < modal.length; i++){
-      if (event.target == modal[i]) {
-        modal[i].style.display = "none";
-        container.style.display = 'flex';
+    closeButton[i].addEventListener('click', () => closeModal(modal[i]));
+
+    window.addEventListener('click', (event) => {
+      for (let i = 0; i < modal.length; i++) {
+        if (event.target == modal[i]) closeModal(modal[i])
       }
-    }
+    })
   }
 }
